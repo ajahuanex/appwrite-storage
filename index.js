2b22ccbe0cf858d762e671d8e658a620c4d9517b
Ajahuana <ajahuana@hotmail.com>
2023-02-22 16:53:44 -0500

    Upload File to AppWrite

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-import { Client, Storage } from "appwrite";
 
-const client = new Client()
-    .setEndpoint('https://[HOSTNAME_OR_IP]/v1')
-    .setProject('[PROJECT_ID]');
+import { Client, Storage, ID, InputFile} from "node-appwrite";
+import * as dotenv from 'dotenv'
+dotenv.config();
 
+// Init SDK
+const client = new Client();
 const storage = new Storage(client);
 
-const promise = storage.createFile(
-    '[BUCKET_ID]',
-    ID.unique(),
-    document.getElementById('uploader').files[0]
-);
+client
+    .setEndpoint(process.env.HOSTNAME_IP) // Your API Endpoint
+    .setProject(process.env.PROJECT_ID) // Your project ID
+    .setKey(process.env.API_KEY) // Your secret API key
+;
+
+const promise = storage.createFile(process.env.BUCKET_ID, ID.unique(), InputFile.fromPath('assets/lista.pdf', 'file6.pdf'));
 
 promise.then(function (response) {
-    console.log(response); // Success
+    console.log(response);
+    console.log('ID :',response['$id']);
 }, function (error) {
-    console.log(error); // Failure
+    console.log(error);
 });
-            
